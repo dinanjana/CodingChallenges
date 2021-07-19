c48dd9ecc375d29039bf20b2a7396c3ac8b58773
dinanjana <dinanjana@fixico.nl>
2021-07-19 07:07:53 +0530

    Max sub array optimal solution

--- LeetCode/MaxSubArray.test.js
+++ LeetCode/MaxSubArray.test.js
@@ -1,17 +1,15 @@
 const maxSubArray = array => {
-    let currentMax = null;
+    let currentMax = Number.MIN_SAFE_INTEGER;
     let currentContinousSum = 0;
-    for(let i = 0; i < array.length - 1; i++) {
-        currentMax = array[i];
-        currentContinousSum = array[i];
-        for(let j = i + 1 ; i < array.length; j++) {
-            currentContinousSum += array[j];
-            if (currentContinousSum < currentMax) {
-                break;
-            } else {
-                currentMax = currentContinousSum;
-            }
+    for(let i = 0; i < array.length; i++) {
+        currentContinousSum += array[i];
+        if(array[i] > currentContinousSum) {
+            currentContinousSum = array[i];
         }
+        if (currentMax < currentContinousSum) {
+            currentMax = currentContinousSum;
+        }
+        
     }
     return currentMax;
 };
@@ -20,4 +18,7 @@ describe("maxSubArray", () => {
     it("[-2,1,-3,4,-1,2,1,-5,4]", () => {
         expect(maxSubArray([-2,1,-3,4,-1,2,1,-5,4])).toBe(6);
     });
+    it("[5,4,-1,7,8]", () => {
+        expect(maxSubArray([5,4,-1,7,8])).toBe(23);
+    })
 })
